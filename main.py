e76c3e50f4f65609a5b14eaa2969cb33ff4ba5fa
Sidorova <liza.sidorova.05@inbox.ru>
2024-04-09 19:45:26 +0600

    car

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+#Получить список 20 последний опубликованных объявлений о продаже автомобилей на сайте https://auto.drom.ru/,
+#результат записать в файл.
 from bs4 import BeautifulSoup # импортируем библиотеку BeautifulSoup
 import requests # импортируем библиотеку requests
 
 def parse():
-    url = 'https://pypi.org/project/bs4/' # передаем необходимы URL адрес
-    page = requests.get(url) # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
+    url = 'https://auto.drom.ru/' # передаем необходимы URL адрес
+    page = requests.get(url)  # отправляем запрос методом Get на данный адрес и получаем ответ в переменную
     print(page.status_code) # смотрим ответ
     soup = BeautifulSoup(page.text, "html.parser") # передаем страницу в bs4
-
-    block = soup.findAll('div', class_='css-1cljngw euytev42') # находим  контейнер с нужным классом
+    block = soup.findAll('div', class_='css-jlnpz8 e1icyw250')  # находим  контейнер с нужным классом
     description = ''
-    for data in block: # проходим циклом по содержимому контейнера
-        if data.find('div'): # находим тег <p>
-            description = data.text # записываем в переменную содержание тегa
-
+    file = open('car_ads.txt', 'w', encoding='UTF-8')
+    for data in block:
+        if data.find('div'):
+            description += data.text.strip() + '\n'
     print(description)
+    file.write(description)
+    file.close()
+parse()
